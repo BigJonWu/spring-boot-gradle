b5470de7600d93631e6b3a051d6352702047a84c
吴强 <171637515@qq.com>
2018-04-20 11:40:07 +0800

    rabbitmq

--- src/main/java/com/example/controller/HelloWorldController.java
+++ src/main/java/com/example/controller/HelloWorldController.java
@@ -1,11 +1,16 @@
 package com.example.controller;
 
 import com.example.domain.User;
+import lombok.extern.slf4j.Slf4j;
 import lombok.val;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+@Slf4j
 @RestController //@RestController 的意思就是controller里面的方法都以json格式输出，不用再写什么jackjson配置的了！
 //设置想要跳转的父路径
 //@RequestMapping(value = "/c")
@@ -15,8 +20,11 @@ public class HelloWorldController {
     //返回的数据不是html标签的页面，而是其他某种格式的数据时（如json、xml等）使用
     @ResponseBody
     public String index() {
-        val customer = new User();
-        customer.setAge(1);
+
+//        SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");//设置日期格式
+//        String date = df.format(new Date());// new Date()为获取当前系统时间，也可使用当前时间戳
+//        log.info("Call Hello Word：" + date);
+
         return "Hello World！";
     }
 }
--- src/main/java/com/example/domain/User.java
+++ src/main/java/com/example/domain/User.java
@@ -1,18 +1,31 @@
 package com.example.domain;
 
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.*;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.NotNull;
 import java.io.Serializable;
+import java.util.Date;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+//@ApiModel
 public class User implements Serializable {
 
     private int id;
 
-    private String Name;
+    @NotNull(message = "名字不能为空")
+    @ApiModelProperty(value = "名字", example = "Jon", required = true)
+    private String name;
 
+    @Max(value = 120, message = "年龄最大不能查过120")
     private int age;
+
+    @Email(message = "邮箱格式错误")
+    private String email;
 }
