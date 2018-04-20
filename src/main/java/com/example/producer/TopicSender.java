47d93224eeefef8d63a7b4f20062269717b6a3b6
吴强 <171637515@qq.com>
2018-04-20 16:30:09 +0800

    fanoutTopicSender

---
+++ src/main/java/com/example/producer/TopicSender.java
@@ -0,0 +1,30 @@
+package com.example.producer;
+
+import org.springframework.amqp.core.AmqpTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class TopicSender {
+
+    @Autowired
+    private AmqpTemplate rabbitTemplate;
+
+    public void send() {
+        String context = "hi, i am message all";
+        System.out.println("Sender : " + context);
+        this.rabbitTemplate.convertAndSend("topicExchange", "topic.1", context);
+    }
+
+    public void sendMessage() {
+        String context = "hi, i am message 1";
+        System.out.println("Sender : " + context);
+        this.rabbitTemplate.convertAndSend("topicExchange", "topic.message", context);
+    }
+
+    public void sendMessages() {
+        String context = "hi, i am messages 2";
+        System.out.println("Sender : " + context);
+        this.rabbitTemplate.convertAndSend("topicExchange", "topic.messages", context);
+    }
+}
